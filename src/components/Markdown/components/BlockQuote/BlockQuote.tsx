bd2782d672d0bfc5448082fc5c4a0b606e44ef7f
柒月流火 <signup@wilfredshen.cn>
2024-08-23 14:38:45 +0800

    fix: 修复 github 风格 note 解析错误的 bug

--- src/components/Markdown/components/BlockQuote/BlockQuote.tsx
+++ src/components/Markdown/components/BlockQuote/BlockQuote.tsx
@@ -2,6 +2,7 @@ import React from "react";
 
 import type { ElementProps } from "@/types/element";
 import { omitNode } from "@/utils/common";
+
 import Note from "../Container/Note";
 
 export interface BlockQuoteProps
@@ -17,40 +18,53 @@ const BlockQuote = (props: BlockQuoteProps) => {
 
 export default BlockQuote;
 
+/**
+ * 解析是否为 Note
+ *
+ * @param props
+ * @returns
+ */
 function parseNote(props: BlockQuoteProps) {
   const { children } = props;
 
   if (!Array.isArray(children)) return;
 
+  /** 找到第一个元素，并且该元素为 p 元素 */
   let firstParagraphIndex = -1;
   for (const [index, item] of children.entries()) {
-    if (typeof item === "object" && item.type === "p") {
-      firstParagraphIndex = index;
+    if (typeof item === "object") {
+      if (item.type === "p") firstParagraphIndex = index;
       break;
     }
   }
-
   if (firstParagraphIndex === -1) return;
 
   const pattern = /^\s*\[!([^\]]*)\](.*)/;
   const firstParagraph: React.ReactElement = children[firstParagraphIndex];
   const restParagraphs = children.slice(firstParagraphIndex + 1);
   if (Array.isArray(firstParagraph.props.children)) {
+    /** 第一个 p 元素有多个子元素 */
+
+    /** 第一个子元素为文本节点时，才可能为 Note */
     const [firstText] = firstParagraph.props.children;
     if (typeof firstText !== "string") return;
 
-    const type = firstText.match(pattern)?.[1];
+    const match = firstText.match(pattern);
+    if (!match) return;
+
+    /** 解析 note 类型，并将剩余部分作为 title 的一部分 */
+    const [_, type, rest] = match;
+    const title: React.ReactNode[] = firstParagraph.props.children.slice(1);
+    if (rest?.trim()) title.unshift(rest.trim());
 
     return (
-      <Note
-        type="note"
-        noteType={type?.trim() || undefined}
-        title={firstParagraph.props.children.slice(1)}
-      >
+      <Note type="note" noteType={type?.trim() || undefined} title={title}>
         {restParagraphs}
       </Note>
     );
   } else if (typeof firstParagraph.props.children === "string") {
+    /** 第一个 p 元素只有单个文本子元素 */
+
     const text: string = firstParagraph.props.children;
     const [_, type, title] = text.match(pattern) ?? [];
 
